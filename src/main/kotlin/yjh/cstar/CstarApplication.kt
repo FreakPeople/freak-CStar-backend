6ad576223a47b2747f5dc099180f0846727d4933
jeongjihun <wlgns2234@naver.com>
2024-08-03 19:14:47 +0900

    init: 프로젝트 세팅

---
+++ src/main/kotlin/yjh/cstar/CstarApplication.kt
@@ -0,0 +1,11 @@
+package yjh.cstar
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CstarApplication
+
+fun main(args: Array<String>) {
+    runApplication<CstarApplication>(*args)
+}
---
+++ src/test/kotlin/yjh/cstar/CstarApplicationTests.kt
@@ -0,0 +1,12 @@
+package yjh.cstar
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CstarApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
