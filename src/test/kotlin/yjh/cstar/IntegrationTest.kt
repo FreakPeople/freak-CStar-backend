3d6f8c46e51140f90ec88f4ede70d241191e32ae
jeongjihun <wlgns2234@naver.com>
2024-08-16 18:48:29 +0900

    style: 사용하지 않는 import문 제거

--- src/test/kotlin/yjh/cstar/IntegrationTest.kt
+++ src/test/kotlin/yjh/cstar/IntegrationTest.kt
@@ -2,7 +2,6 @@ package yjh.cstar
 
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
-import org.springframework.test.context.jdbc.Sql
 import org.springframework.transaction.annotation.Transactional
 
 @Transactional
