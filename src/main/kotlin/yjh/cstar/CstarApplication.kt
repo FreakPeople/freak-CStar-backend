5bdbc23b87a7726152b5c30cc0b7e319e62c0af6
Lea Hwang <dbwjd5032@gmail.com>
2024-08-25 00:21:28 +0900

    feat: 스프링 비동기 이벤트 설정 추가

--- src/main/kotlin/yjh/cstar/CstarApplication.kt
+++ src/main/kotlin/yjh/cstar/CstarApplication.kt
@@ -2,9 +2,7 @@ package yjh.cstar
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
-import org.springframework.scheduling.annotation.EnableAsync
 
-@EnableAsync
 @SpringBootApplication
 class CstarApplication
 
