d433211c5937d912af96c618a3233de7840d3d83
jeongjihun <wlgns2234@naver.com>
2024-08-24 16:56:47 +0900

    refactor: equals(), hashcode()를 위해 data 클래스로 변경

--- src/main/kotlin/yjh/cstar/quiz/infrastructure/jpa/GameQuizEntity.kt
+++ src/main/kotlin/yjh/cstar/quiz/infrastructure/jpa/GameQuizEntity.kt
@@ -9,28 +9,10 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.io.Serializable
 
 @Embeddable
-class GameQuizId(
+data class GameQuizId(
     val gameId: Long = 0,
     val quizId: Long = 0,
-) : Serializable {
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as GameQuizId
-
-        if (gameId != other.gameId) return false
-        if (quizId != other.quizId) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = gameId.hashCode()
-        result = 31 * result + quizId.hashCode()
-        return result
-    }
-}
+) : Serializable
 
 @EntityListeners(AuditingEntityListener::class)
 @Table(name = "game_quiz")
