a1241ad03403aa82cceb7cf67181f2c7009558dd
jeongjihun <wlgns2234@naver.com>
2024-08-24 12:29:38 +0900

    feat: jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,11 @@ dependencies {
     // mysql
     runtimeOnly("com.mysql:mysql-connector-j")
 
+    // jwt
+    runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+    runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+
     // logging
     implementation("io.github.oshai:kotlin-logging-jvm:5.1.4")
 
