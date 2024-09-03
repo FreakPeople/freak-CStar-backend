66ba6951142042580a0229e4683fe1ec02b23a8a
Lea Hwang <dbwjd5032@gmail.com>
2024-09-03 14:21:31 +0900

    refactor: CORS 허용 주소 변경
    - 프론트를 S3에 배포함에따라 주소가 변경되어 해당 주소로 CORS 허용 주소를 변경합니다.
    - 해당 주소를 application.yml파일로 관리합니다.

--- src/main/kotlin/yjh/cstar/config/WebConfig.kt
+++ src/main/kotlin/yjh/cstar/config/WebConfig.kt
@@ -1,13 +1,18 @@
 package yjh.cstar.config
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
+
+    @Value("\${cors.src}")
+    lateinit var src: String
+
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins(src)
     }
 }
