88384a2740b816b95492b7bbed4d09a186481d09
Lea Hwang <dbwjd5032@gmail.com>
2024-09-15 20:02:27 +0900

    refactor: game engine 서비스 패키지 분리에 따른 import 추가

--- src/main/kotlin/yjh/cstar/game/application/GameService.kt
+++ src/main/kotlin/yjh/cstar/game/application/GameService.kt
@@ -2,6 +2,7 @@ package yjh.cstar.game.application
 
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import yjh.cstar.engine.application.GameEngineService
 import yjh.cstar.game.domain.GameStartCommand
 import yjh.cstar.quiz.application.QuizService
 import yjh.cstar.room.application.RoomService
