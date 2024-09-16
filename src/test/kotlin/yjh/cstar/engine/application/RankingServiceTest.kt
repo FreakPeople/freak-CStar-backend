f0e5f03213f69b04b0ccbfbec081c42d55412895
Lea Hwang <dbwjd5032@gmail.com>
2024-09-17 02:07:07 +0900

    test: Ranking 도메인 객체로 랭킹 집계 로직 분리 후 테스트

--- src/test/kotlin/yjh/cstar/engine/application/RankingServiceTest.kt
+++ src/test/kotlin/yjh/cstar/engine/application/RankingServiceTest.kt
@@ -2,9 +2,12 @@ package yjh.cstar.engine.application
 
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import org.springframework.beans.factory.annotation.Autowired
 import yjh.cstar.IntegrationTest
-import java.util.*
+import yjh.cstar.common.BaseException
+import yjh.cstar.engine.domain.Ranking
+import java.util.TreeMap
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 
@@ -17,19 +20,21 @@ class RankingServiceTest : IntegrationTest() {
     @Test
     fun `퀴즈별 플레이어 랭킹 계산 테스트`() {
         // given
-        val ranking = TreeMap<Long, Int>()
-        val nicknames = mutableMapOf<Long, String>()
-
-        ranking[1L] = 2
-        ranking[2L] = 3
-        ranking[3L] = 1
-
-        nicknames[1L] = "플레이어1"
-        nicknames[2L] = "플레이어2"
-        nicknames[3L] = "플레이어3"
+        val players = listOf(1L, 2L, 3L)
+        val playerScores = TreeMap<Long, Int>().apply {
+            put(1L, 2)
+            put(2L, 3)
+            put(3L, 1)
+        }
+        val nicknames = mutableMapOf<Long, String>().apply {
+            put(1L, "플레이어1")
+            put(2L, "플레이어2")
+            put(3L, "플레이어3")
+        }
+        val ranking = Ranking(players, playerScores)
 
         // when
-        val rankingMessage = rankingService.generateRanking(ranking, nicknames)
+        val rankingMessage = rankingService.getRankingMessage(ranking, nicknames)
 
         // then
         val expected = "[1등 플레이어2-3]  [2등 플레이어1-2]  [3등 플레이어3-1]  "
@@ -39,14 +44,16 @@ class RankingServiceTest : IntegrationTest() {
     @Test
     fun `최종 게임 결과 계산 테스트 - 최종 1등인 플레이어 아이디를 반환한다`() {
         // given
-        val ranking = TreeMap<Long, Int>()
-
-        ranking[1L] = 2
-        ranking[2L] = 3
-        ranking[3L] = 1
+        val players = listOf(1L, 2L, 3L)
+        val playerScores = TreeMap<Long, Int>().apply {
+            put(1L, 2)
+            put(2L, 3)
+            put(3L, 1)
+        }
+        val ranking = Ranking(players, playerScores)
 
         // when
-        val winningPlayerId = rankingService.calculateGameResult(ranking)
+        val winningPlayerId = ranking.getWinnerId()
 
         // then
         assertNotNull(winningPlayerId)
@@ -54,14 +61,25 @@ class RankingServiceTest : IntegrationTest() {
     }
 
     @Test
-    fun `최종 게임 결과 계산 테스트 - 아무도 퀴즈를 풀지 못했을 경우 -1을 반환한다 `() {
+    fun `최종 게임 결과 계산 테스트 - 아무도 퀴즈를 풀지 못했을 경우 -1을 반환한다`() {
         // given
-        val ranking = TreeMap<Long, Int>()
+        val players = listOf(1L, 2L, 3L)
+        val ranking = Ranking(players)
 
         // when
-        val winningPlayerId = rankingService.calculateGameResult(ranking)
+        val winningPlayerId = ranking.getWinnerId()
 
         // then
-        assertEquals(winningPlayerId, -1)
+        assertEquals(-1, winningPlayerId)
+    }
+
+    @Test
+    fun `최종 게임 결과 계산 테스트 - 플레이어가 존재하지 않을 경우 예외를 반환한다`() {
+        // given
+        val players = emptyList<Long>()
+        val ranking = Ranking(players)
+
+        // when & then
+        assertThrows<BaseException> { ranking.getWinnerId() }
     }
 }
