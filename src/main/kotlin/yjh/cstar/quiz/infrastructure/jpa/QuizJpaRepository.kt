d414cc25d9ae1d19f82f04cedc36d6146bf6fa97
jeongjihun <wlgns2234@naver.com>
2024-10-02 20:19:18 +0900

    feat: 랜덤 데이터를 가져오도록 수정
    - MySQL의 rand() 함수를 썼는데, 문제 없을지 검토가 필요해 보입니다.

--- src/main/kotlin/yjh/cstar/quiz/infrastructure/jpa/QuizJpaRepository.kt
+++ src/main/kotlin/yjh/cstar/quiz/infrastructure/jpa/QuizJpaRepository.kt
@@ -13,7 +13,7 @@ interface QuizJpaRepository : JpaRepository<QuizEntity, Long> {
             FROM quiz
             WHERE category_id = :quizCategoryId
                 AND deleted_at IS NULL
-            ORDER BY created_at DESC
+            ORDER BY RAND()
             LIMIT :totalQuestions
         """,
         nativeQuery = true
@@ -31,17 +31,17 @@ interface QuizJpaRepository : JpaRepository<QuizEntity, Long> {
 
     @Query(
         value = """
-        SELECT q.*
-        FROM quiz q
-        WHERE q.deleted_at IS NULL
-          AND q.quiz_id IN (
-              SELECT gq.quiz_id
-              FROM game_quiz gq
-              JOIN member_game_result mgr ON mgr.game_id = gq.game_id
-              WHERE mgr.member_id = :memberId
-                AND mgr.total_count > 0
-          )
-    """,
+            SELECT q.*
+            FROM quiz q
+            WHERE q.deleted_at IS NULL
+              AND q.quiz_id IN (
+                  SELECT gq.quiz_id
+                  FROM game_quiz gq
+                  JOIN member_game_result mgr ON mgr.game_id = gq.game_id
+                  WHERE mgr.member_id = :memberId
+                    AND mgr.total_count > 0
+              )
+        """,
         nativeQuery = true
     )
     fun findAllAttemptedByMember(
