43f7fd9fb5fa38398794e18dece94f86ca845100
jeongjihun <wlgns2234@naver.com>
2024-10-03 02:02:48 +0900

    refactor: 정답 판단 로직 개선
    - 대소문자와 띄어쓰기를 구별하지 않고, 정답을 판별하도록 개선

--- src/test/kotlin/yjh/cstar/play/domain/quiz/QuizTest.kt
+++ src/test/kotlin/yjh/cstar/play/domain/quiz/QuizTest.kt
@@ -1,10 +1,12 @@
 package yjh.cstar.play.domain.quiz
 
+import org.junit.jupiter.api.Assertions.assertFalse
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertDoesNotThrow
-import kotlin.test.assertFalse
-import kotlin.test.assertTrue
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+import kotlin.test.assertEquals
 
 @DisplayName("[Domain 테스트] Quiz")
 class QuizTest {
@@ -20,28 +22,28 @@ class QuizTest {
         assertDoesNotThrow { Quiz.of(quizId, question, answer) }
     }
 
-    @Test
-    fun `같은 정답을 가지고 있는지 테스트`() {
+    @ParameterizedTest
+    @CsvSource(
+        "정답, 정 답,true",
+        "정   답, 정답, true",
+        "abc, ABC, true" // 공백이 포함된 경우 등 다양한 케이스도 추가 가능
+    )
+    fun `같은 정답을 가지고 있는지 테스트`(answer: String, playerAnswer: String, expected: Boolean) {
         // given
-        val quizId = 1L
-        val question = "질문"
-        val answer = "정답"
-        val quiz = Quiz(quizId, question, answer)
+        val quiz = Quiz(1L, "question", answer)
 
         // when
-        val isSameAnswer = quiz.isSameAnswer("정답")
+        val isSameAnswer = quiz.isSameAnswer(playerAnswer)
 
         // then
-        assertTrue(isSameAnswer)
+        assertEquals(expected, isSameAnswer)
     }
 
     @Test
     fun `다른 정답을 가지고 있는지 테스트`() {
         // given
-        val quizId = 1L
-        val question = "질문"
         val answer = "정답"
-        val quiz = Quiz(quizId, question, answer)
+        val quiz = Quiz(1L, "질문", answer)
 
         // when
         val isSameAnswer = quiz.isSameAnswer("잘못된 정답")
