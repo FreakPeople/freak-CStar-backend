889477e4bf995a318b53d4b4a827d5c89cd1911e
Lea Hwang <dbwjd5032@gmail.com>
2025-02-16 19:04:17 +0900

    chore: common 모듈 에러코드 관련 주석

--- cstar-common/src/main/kotlin/yjh/cstar/common/exception/BaseErrorCode.kt
+++ cstar-common/src/main/kotlin/yjh/cstar/common/exception/BaseErrorCode.kt
@@ -2,6 +2,9 @@ package yjh.cstar.common.exception
 
 import org.springframework.http.HttpStatus
 
+/**
+ * 각 모듈에서 BaseErrorCode 인터페이스를 구현해 사용합니다
+ */
 interface BaseErrorCode {
     val message: String
     val httpStatus: HttpStatus
