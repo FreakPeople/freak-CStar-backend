10961b293a0dedaa746ee3f9699243a7d9830f50
Lea Hwang <dbwjd5032@gmail.com>
2025-02-25 11:08:07 +0900

    feat: 모니터링 설정 추가
    - 프로메테우스, 그라파나 docker-compose 설정 파일에 추가

--- cstar-api/build.gradle.kts
+++ cstar-api/build.gradle.kts
@@ -27,6 +27,9 @@ dependencies {
     implementation("org.flywaydb:flyway-core")
     implementation("org.flywaydb:flyway-mysql")
 
+    // prometheus
+    implementation("io.micrometer:micrometer-registry-prometheus")
+
     // mysql
     runtimeOnly("com.mysql:mysql-connector-j")
 
